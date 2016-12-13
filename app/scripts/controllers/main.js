d6b49cb81bdc1ad85f90e311489b1c618dcf2403
Michael Staehling <Michael.staehling@gmail.com>
2016-12-13 15:02:06 -0500

    fixed syntax errors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,11 +10,11 @@
 angular.module('flickrTestApp')
   .controller('MainCtrl', function () {
     this.search = function () {
-      console.log("searching");
+      console.log('searching');
     };
-
+    // https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=05138fb5dbd2cce1ba2b4e069a11efa8&text=tree&safe_search=1&per_page=25&format=rest
   })
-  .directive('ngEnter', function($location) {
+  .directive('ngEnter', function() {
     return function(scope, element, attrs) {
       element.bind('keydown keypress', function(event) {
         if (event.which === 13) {
