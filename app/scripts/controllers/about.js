7346a73c42bfd1bef5711d3e39f4d03fe4e1235a
Michael Staehling <Michael.staehling@gmail.com>
2016-12-13 12:52:48 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flickrTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flickrTestApp
+ */
+angular.module('flickrTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
