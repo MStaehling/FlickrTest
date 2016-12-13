1903f9ff8a166495b3ad1cd2239988c9010290d7
Michael Staehling <Michael.staehling@gmail.com>
2016-12-13 17:06:48 -0500

    making api call to Flickr

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,23 @@
  * Controller of the flickrTestApp
  */
 angular.module('flickrTestApp')
-  .controller('MainCtrl', function () {
-    this.search = function () {
+  .controller('MainCtrl', function($http, $q) {
+    var deferred = $q.defer();
+    var v = this;
+    this.search = function(searchText) {
+      $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f14826d323490270cacd02d9dc7d5a73&text=' + searchText + '&safe_search=1&per_page=25&format=json&nojsoncallback=1')
+        .then(function(res) {
+          if (res.status === 200) {
+            v.photos = res.data;
+            console.log(res.data);
+            deferred.resolve();
+          } else {
+            deferred.reject();
+          }
+          return deferred.promise;
+        });
       console.log('searching');
+      //return deferred.promise;
     };
     // https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=05138fb5dbd2cce1ba2b4e069a11efa8&text=tree&safe_search=1&per_page=25&format=rest
   })
